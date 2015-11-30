751a177475f395ba93eccc3bc2e5e6430b6d77fb
Wassim Dhif <wassimdhif@gmail.com>
2015-11-30 17:51:38 +0100

    Add C++ Sources

---
+++ Source/MazeEXL/MazeEXLGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "MazeEXLGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMazeEXLGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AMazeEXLGameMode();
+};
+
+
+
